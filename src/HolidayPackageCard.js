b31f7efb5abd381ea3b265d527fc9af77a61f700
Akhilesh Singh <akhileshsinghjij@gmail.com>
2024-09-30 17:52:11 +0530

    data change

--- src/HolidayPackageCard.js
+++ src/HolidayPackageCard.js
@@ -9,21 +9,201 @@ const HolidayPackageCard = () => {
 
   useEffect(() => {
     // API call
-    const fetchData = async () => {
+    const fetchData = () => {
       try {
-        const response = await axios.get(
-          'http://172.105.55.211/api/packageApi.php?type=getHolidayPackage',
-          {
-            headers: {
-              Authorization: `Bearer ${AUTH_TOKEN}`, // Setting the auth header
+        const response = {
+          isSuccess: true,
+          successMessage: "get Holiday Package Successfully.",
+          errorMessage: null,
+          data: [
+            {
+              id: 17,
+              user_id: 11,
+              title: "greenland",
+              no_of_days: 9,
+              destination: "punjab",
+              hotel_cat: "budget",
+              plan_type: "MAP",
+              include: [
+                "sightseeing"
+              ],
+              transport_type: null,
+              start_from: [
+                "3"
+              ],
+              description: "lovely place to visit. ",
+              price: 68,
+              price_per: "per_person",
+              cab_name: null,
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1722671597105483"
+              ],
+              active: 1,
+              created_at: "2024-07-17 06:19:21",
+              updated_at: "2024-07-17 06:19:21",
+              is_wishlist: 0
             },
-          }
-        );
+            {
+              id: 20,
+              user_id: 11,
+              title: "Valley of flowers ",
+              no_of_days: 20,
+              destination: "raipur",
+              hotel_cat: "4star",
+              plan_type: "CP",
+              include: [
+                "sightseeing",
+                "transport"
+              ],
+              transport_type: "hatchback",
+              start_from: [
+                "New Delhi"
+              ],
+              description: "thhiikll",
+              price: 23,
+              price_per: "per_couple",
+              cab_name: "Teat",
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1000032901.jpg"
+              ],
+              active: 1,
+              created_at: "2024-07-17 06:35:47",
+              updated_at: "2024-07-17 06:35:47",
+              is_wishlist: 0
+            },
+            {
+              id: 22,
+              user_id: 11,
+              title: "Ruph Kund Uttarakhand Trip",
+              no_of_days: 6,
+              destination: "Ruph Kund",
+              hotel_cat: "3star",
+              plan_type: "MAP",
+              include: [
+                "sightseeing"
+              ],
+              transport_type: null,
+              start_from: [
+                "New Delhi"
+              ],
+              description: "thncfhh",
+              price: 34,
+              price_per: "per_couple",
+              cab_name: null,
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1000032900.webp"
+              ],
+              active: 1,
+              created_at: "2024-07-17 06:40:51",
+              updated_at: "2024-07-17 06:40:51",
+              is_wishlist: 0
+            },
+            {
+              id: 23,
+              user_id: 11,
+              title: "Ganga Ghat Patna",
+              no_of_days: 10,
+              destination: "Patna Bihar",
+              hotel_cat: "5star",
+              plan_type: "CP",
+              include: [
+                "sightseeing"
+              ],
+              transport_type: null,
+              start_from: [
+                "tbjk"
+              ],
+              description: "th nkllnabachkam",
+              price: 34,
+              price_per: "per_couple",
+              cab_name: null,
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1722674498982574"
+              ],
+              active: 1,
+              created_at: "2024-07-17 06:42:36",
+              updated_at: "2024-07-17 06:42:36",
+              is_wishlist: 0
+            },
+            {
+              id: 45,
+              user_id: 11,
+              title: "Test",
+              no_of_days: 2,
+              destination: "Dubai",
+              hotel_cat: "5star",
+              plan_type: "AP",
+              include: [],
+              transport_type: null,
+              start_from: null,
+              description: "this is a test",
+              price: 5000,
+              price_per: "per_person",
+              cab_name: null,
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1727454905779200"
+              ],
+              active: 1,
+              created_at: "2024-09-27 16:35:05",
+              updated_at: "2024-09-27 16:35:05",
+              is_wishlist: 0
+            },
+            {
+              id: 46,
+              user_id: 11,
+              title: "Test Package ",
+              no_of_days: 12,
+              destination: "Noida",
+              hotel_cat: "5star",
+              plan_type: "CP",
+              include: [],
+              transport_type: null,
+              start_from: null,
+              description: "noida",
+              price: 5000,
+              price_per: "per_person",
+              cab_name: null,
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1727493798598195"
+              ],
+              active: 1,
+              created_at: "2024-09-28 03:23:18",
+              updated_at: "2024-09-28 03:23:18",
+              is_wishlist: 0
+            },
+            {
+              id: 47,
+              user_id: 11,
+              title: "Noida",
+              no_of_days: 25,
+              destination: "Nodja",
+              hotel_cat: "4star",
+              plan_type: "MAP",
+              include: [
+                "transport"
+              ],
+              transport_type: "sedan",
+              start_from: null,
+              description: "bdjdje",
+              price: 500,
+              price_per: "per_person",
+              cab_name: "bdid",
+              images: [
+                "http://172.105.55.211/images/holidaypackage/1727493935447343"
+              ],
+              active: 1,
+              created_at: "2024-09-28 03:25:35",
+              updated_at: "2024-09-28 03:25:35",
+              is_wishlist: 0
+            }
+          ]
+        }
+
+        console.log(response)
+        setPackages(response.data);
         if (response.data && response.data.isSuccess && response.data.data.length > 0) {
           console.log(response.data.data);
           setPackages(response.data.data); // Access the `data` array
-        } else {
-          setError('No packages found.');
         }
       } catch (error) {
         console.error('Error fetching the packages:', error);
